104b37a6b7f446c5331241785e37d37898d7b328
imnana18 <n_chena2@outlook.com>
2024-04-02 12:04:41 -0400

    Anna's Update
    Added SVM and NN models to the main Fetal_Health_Prediction.ipynb file. Updated application base code.

--- App/hospital.js
+++ App/hospital.js
@@ -1,68 +1,87 @@
+const searchBar = document.getElementById("searchBabyID");
+console.log(searchBar)
 
-const dropdown = d3.select("#sel-dataset");
+let data
 
-console.log(dropdown)
+// Data retrieval
+async function dataPull(){
+    // define the URL of your JSON file
+    const jsonPath = 'fetal_data.json';
+  
+    // promise Pending
+    const dataPromise = d3.json(jsonPath);
+  
+    // fetch JSON data and console log
+    await dataPromise;
 
-function optionChanged(newID) {
-    console.log("Selected ID: ", newID);
+    // return fetched data
+    return dataPromise;
   }
 
-// Data retrieval function
-async function csvRead(){
-    try {
-    // Use d3.csv() to load and parse the CSV file
-    const data = await d3.csv("../Data/fetal_health.csv");
 
-    // 'data' contains the parsed CSV data as an array of objects
-    console.log(data);
-    return data;
-
-    } catch (error) {
-        // Handle any errors that occur during loading
-        console.error("Error loading the CSV file:", error);
-        throw error; // rethrow the error to propagate it
-    }
-}
-
-
-
-// Populate dropdown menu
-function dropDownInit(csvData){
-    
-    for (var i = 1; i < csvData.length; i++) {
-        const option = document.createElement("option");
-
-        console.log(option)
-        console.log(dropdown)
-        // Set the value and text content of the option element
-        option.value = "Option Value";
-        option.textContent = "Option Text";
-
-        // Append the option element to the select element
-        dropdown.append(option);
-        
-    }
-    console.log(dropdown)
-    return dropdown
+// Function to handle search by baby ID
+function searchBaby() {
+    // Get the value entered in the search bar
+    const babyID = searchBar.value;
+    console.log(babyID)
+    displayBabyInfo(babyID);
 };
 
+// Function to display baby information
+function displayBabyInfo(babyID) {
+    // Example: Display baby information in the "sample-metadata" div
+    const metadataDiv = document.getElementById("sample-metadata");
+    
+    console.log(data)
+    const searchedBaby = data[babyID]
+    console.log()
+    console.log(searchedBaby)
+    metadataDiv.innerHTML = `<h4>Baby ID: ${babyID}</h4>` 
+    +`\n<h4>FHR_hist_max : ${searchedBaby['FHR_hist_max']} </h4>`
+    +`\n<h4>FHR_hist_mean : ${searchedBaby['FHR_hist_mean']} </h4>`
+    +`\n<h4>FHR_hist_median : ${searchedBaby['FHR_hist_median']} </h4>`
+    +`\n<h4>FHR_hist_min : ${searchedBaby['FHR_hist_min']} </h4>`
+    +`\n<h4>FHR_hist_mode : ${searchedBaby['FHR_hist_mode']} </h4>`
+    +`\n<h4>FHR_hist_num_peaks : ${searchedBaby['FHR_hist_num_peaks']} </h4>`
+    +`\n<h4>FHR_hist_num_zeroes : ${searchedBaby['FHR_hist_num_zeroes']} </h4>`
+    +`\n<h4>FHR_hist_tendency : ${searchedBaby['FHR_hist_tendency']} </h4>`
+    +`\n<h4>FHR_hist_variance : ${searchedBaby['FHR_hist_variance']} </h4>`
+    +`\n<h4>FHR_hist_width : ${searchedBaby['FHR_hist_width']} </h4>`
+    +`\n<h4>abnorm_LT_Var_Perc : ${searchedBaby['abnorm_LT_Var_Perc']} </h4>`
+    +`\n<h4>abnorm_ST_Var_Perc : ${searchedBaby['abnorm_ST_Var_Perc']} </h4>`
+    +`\n<h4>accelerations : ${searchedBaby['accelerations']} </h4>`
+    +`\n<h4>baseline_FHR_bpm : ${searchedBaby['baseline_FHR_bpm']} </h4>`
+    +`\n<h4>fetal_movement : ${searchedBaby['fetal_movement']} </h4>`
+    +`\n<h4>light_decelerations : ${searchedBaby['light_decelerations']} </h4>`
+    +`\n<h4>mean_LT_Var : ${searchedBaby['mean_LT_Var']} </h4>`
+    +`\n<h4>mean_ST_Var : ${searchedBaby['mean_ST_Var']} </h4>`
+    +`\n<h4>prolonged_decelerations : ${searchedBaby['prolonged_decelerations']} </h4>`
+    +`\n<h4>severe_decelerations : ${searchedBaby['severe_decelerations']} </h4>`
+    +`\n<h4>uterine_contractions : ${searchedBaby['uterine_contractions']} </h4>`
+}
 
 // Init function
 async function init(){
- 
+
     // create variable for JSON data after data is pulled
-    csvData = await csvRead()
-
-    console.log(csvData)
-    console.log(csvData.length)
-    // Add array numbers as options
-
-    const dropdown = dropDownInit(csvData)
-
- 
-    
-    
-
-};
+    data = await dataPull()
   
+    // create variable for each data col
+    const babyID = data.map(row => row['Fetal ID']);
+    const babyData = data
+    console.log(babyData)
+    console.log(babyID); // Print the 'Fetal ID' column
+
+    var input = document.getElementById("searchBabyID");
+    input.addEventListener("keyup", function(event) {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        searchBaby();
+      }
+    });
+    
+    // call default initial ID
+    const initSelectID = babyID[0]
+}
+
 init()
